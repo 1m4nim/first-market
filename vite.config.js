0b316a7a858296264921a18d2503f07cbeec2c28
south <fancustard9@gmail.com>
2024-10-22 12:26:00 +0900

    デプロイの問題

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   server: {
     https: false,  // HTTPSを無効化し、HTTPでサーバーを起動する
     proxy: {
